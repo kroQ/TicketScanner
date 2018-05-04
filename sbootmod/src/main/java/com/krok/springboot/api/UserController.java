002d684ed783ead095701b1a89a27b5f53d8be4d
Mateusz Krok <morvidkrk@gmail.com>
2018-05-04 14:15:24 +0200

    Return user object in GET request

--- sbootmod/src/main/java/com/krok/springboot/api/UserController.java
+++ sbootmod/src/main/java/com/krok/springboot/api/UserController.java
@@ -1,6 +1,8 @@
 package com.krok.springboot.api;
 
+import com.krok.error.AppError;
 import com.krok.error.AppException;
+import com.krok.error.DAOError;
 import com.krok.springboot.data.UserData;
 import com.krok.springboot.dto.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ import java.util.logging.Logger;
 
 import javax.persistence.NoResultException;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 /**
  * Created by Mateusz Krok on 2018-04-13
  */
@@ -22,12 +27,12 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    private static Logger LOGGER = Logger.getLogger(
+    private static Logger logger = Logger.getLogger(
             Thread.currentThread().getStackTrace()[0].getClassName());
 
     // ******************** USER API ******************* //
 
-    @RequestMapping("/user/{id}")
+    @RequestMapping(value = "/user/{id}", method = GET)
     public String findUser(@PathVariable("id") int id) {
         try {
             UserData user = userService.getUserById(id);
@@ -35,6 +40,22 @@ public class UserController {
         } catch (NoResultException e) {
             return "No user find";
         }
+    }
+
+    @RequestMapping(value = "/user/2/{id}", method = GET)
+    public UserData findUser2(@PathVariable("id") int id) {
+        try {
+            UserData user = userService.getUserById(id);
+            return user;
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "/user/{id}", method = DELETE)
+    public String deleteUserById(@PathVariable("id") int id) {
+        boolean isDeleted = userService.deleteUserById(id);
+        return isDeleted ? "Deleted: " + id : "User not exist";
     }
 
     @RequestMapping("/user/generate/{login}")
@@ -45,10 +66,24 @@ public class UserController {
         try {
             userService.createOrUpdate(user);
         } catch (AppException e) {
-            LOGGER.info(e.getCodeMessage());
+            logger.info(e.getCodeMessage());
             return "nope: " + e.getMessage() + "\n\n";
         }
         return user.toString();
+    }
+
+    @RequestMapping("/user/generate/2/{login}")
+    public UserData newUser2(@PathVariable("login") String login) throws AppException {
+        // TODO RequestBody and ResponseBody
+        UserData user;
+        user = new UserData("N", "S", "P", "E", login, 1);
+        try {
+            userService.createOrUpdate(user);
+        } catch (AppException e) {
+            logger.info(e.getCodeMessage());
+            return null;
+        }
+        return user;
     }
 
     @RequestMapping("/user/delete/{id}")
